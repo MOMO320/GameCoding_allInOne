4fc77f54b75641b8497a0434e7f78851cfc2b60a
YuYoungHwan <yoodud@naver.com>
2024-05-31 22:14:43 +0900

    Section6 연결리스트 자료구조 정리

---
+++ section6/6_1LinearDataCode/Maze/List.h
@@ -0,0 +1,163 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+template<typename T>
+class Node
+{
+	// typedef int T;	// (옛날 방법)별칭
+	//using T = int;		// (요즘 방법)별칭
+public:
+	Node(T data) : _data(data), prev(nullptr), next(nullptr) { }
+
+public:
+	T		_data;
+	Node*	prev;
+	Node*	next;
+};
+
+
+template<typename T>
+class List
+{
+public:
+	List()
+	{
+		_head = new Node<T>(0);
+		_tail = new Node<T>(0);
+
+		_head->next = _tail;
+		_tail->prev = _head;
+	}
+
+	~List()
+	{
+		Node<T>* node = _head;
+
+		while (node != nullptr)
+		{
+			Node<T>* deleteNode = node;
+			node = node->next;
+			delete deleteNode;
+		}
+	}
+
+
+	// [dummy]<->[1]<->[2]<->[3]<->[dummy]
+	// [head]                       [tail]
+	Node<T>* GetNode(int index)
+	{
+		Node<T>* node = _head->next;
+
+		if (node == _tail)
+			return nullptr;
+
+		for (int i = 0; i < index; i++)
+		{
+			if (node == _tail->prev)
+				return nullptr;
+
+				node = node->next;
+		}
+
+		return node;
+	}
+
+	void Print()
+	{
+		Node<T>* node = _head->next;
+
+		while (node != _tail)
+		{
+			std::cout << node->_data << std::endl;
+			node = node->next;
+		}
+		std::cout << std::endl;
+	}
+
+
+	//       [node]
+	// [dummy]<->[nextNode]<->[2]<->[3]<->[dummy]
+	// [head]                         [tail]
+	Node<T>* AddHead(int data)
+	{
+		Node<T>* node = new Node<T>(data);
+	
+		Node<T>* nextNode = _head->next;
+			
+		// 더미를 활용한 코드
+		node->next = nextNode;
+		nextNode->prev = node;
+		_head->next = node;
+		node->prev = _head;
+
+
+		{ // 더미 노드 없을때 아래처럼
+			//if (_head == nullptr)
+			//{
+			//	_head = node;
+			//	_tail = node;
+			//}
+			//else
+			//{
+			//	Node* nextNode = _head;
+			//	node->next = nextNode;
+			//	nextNode->prev = node;
+			//	_head = node;
+			//}
+		}
+
+		return node;
+	}
+
+	//								  [node]
+	// [dummy]<->[1]<->[2]<->[prevNode]<->[dummy]
+	// [head]                         [tail]
+	Node<T>* AddAtaTail(int data)
+	{
+		Node<T>* node = new Node<T>(data);
+		Node<T>* prevNode = _tail->prev;
+
+		prevNode->next = node;
+		node->prev = prevNode;
+		node->next = _tail;
+		_tail->prev = node;
+
+		return node;
+	}
+	
+	//					  [node]
+	// [dummy]<->[prevNode]<->[posNode]<->[3]<->[4]<->[dummy]
+	// [head]                                [tail]
+	void Insert(Node<T>* posNode, int data)
+	{
+		Node<T>* node = new Node<T>(data);
+		Node<T>* prevNode = posNode->prev;
+
+		prevNode->next = node;
+		node->prev = prevNode;
+		node->next = posNode;
+		posNode->prev = node;
+	}
+
+	//						// 삭제
+	// [dummy]<->[prevNode]<->[node]<->[nextNode]<->[3]<->[dummy]
+	// [head]                                [tail]
+	void Remove(Node<T>* node)
+	{
+		Node<T>* prveNode = node->prev;
+		Node<T>* nextNode = node->next;
+		prveNode->next = nextNode;
+		nextNode->prev = prveNode;
+
+		delete node;
+	}
+
+	/* 특정 노드를 알고 있을때, 중간 삽입, 삭제가 용이 하다는 뜻 */
+	
+private:
+	Node<T>* _head = nullptr;
+	Node<T>* _tail = nullptr;
+	int _count = 0;
+};
+
---
+++ section6/6_1LinearDataCode/Maze/Maze.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include "List.h"
+#include "Array.h"
+using namespace std;
+
+
+int main()
+{
+	List<int> Li;
+
+	Li.AddAtaTail(1);
+
+	Node<int>* node = Li.AddAtaTail(2);
+	Li.AddAtaTail(3);
+	Li.AddAtaTail(4);
+	Li.AddAtaTail(5);
+
+	Li.Print();
+
+	// 특정 위치 알고 있을때 빠르다.
+	Li.Remove(node);
+
+	Li.Print();
+
+
+	Array arr(100);
+	arr.push_back(1);
+	arr.push_back(2);
+	arr.push_back(3);
+	arr.push_back(4);
+
+	arr[1] = 2;
+
+	int b = arr[10000];
+}
